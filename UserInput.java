eb6ff34f52ccfe469090331472a40f7bc71b1e2a
Shubhamkgupta12799 <71540218+Shubhamkgupta12799@users.noreply.github.com>
2022-11-26 10:35:13 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,800 @@
+package fundamentals;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc  = new Scanner(System.in);
+
+		//		int a = sc.nextInt();
+		//		String str = sc.next();
+		//		System.out.print(a);
+		//		System.out.println(str);
+
+		//		float f = 2.4;
+		//
+		//		char ch = sc.next().charAt(0);
+		//		int a = sc.nextInt();
+		//		int b = sc.nextInt();
+		//		int c = sc.nextInt();
+		//
+		//		System.out.println(ch);
+		//		System.out.println((a+b+c)/3);
+		//		int i = 'c';
+
+		//		byte b = 50;
+		//		b=b*50;
+		//		System.out.println(b);
+
+		//		 double a = 6 / 4;
+		//	        int b  = 6 / 4;
+		//	        double c = a + b;
+		//	        System.out.println(c);
+
+		//		 double a = 55.5;
+		//	        int b = 55;
+		//	        a = a % 10;
+		//	        b = b % 10;
+		//	        System.out.println(a + " "  + b);
+
+		//		int a=50;
+		//		if(a>10)
+		//		{
+		//		    System.out.print("Coding");
+		//		}
+		//		else
+		//		{
+		//		    System.out.print("Ninjas");
+		//		}
+
+		//		 int x = 5; 
+		//	        if (x < 6)
+		//	            System.out.print("Hello  ");
+		//	        if(x == 5){
+		//	            System.out.print("Hi  ");
+		//	        }
+		//	        else{ 
+		//	            System.out.print("Hey ");
+		//	        } 
+
+		//		int x = 15;
+		//	    if(x <= 15){
+		//	        System.out.print("Inside if  ");
+		//	    }else if(x == 15){
+		//	        System.out.print("Inside else if  ");
+		//	    }
+		//	    System.out.println(x);
+
+		//		 int var1 = 5; 
+		//	        int var2 = 6;
+		//	        if ((var2 = 1) == var1)
+		//	            System.out.print(var2);
+		//	        else 
+		//	            System.out.print(var2 + 1);
+
+
+
+		//		int a=50;
+		//		int b=Integer.MIN_VALUE;
+		//		if(a/0==b)
+		//		{
+		//
+		//			System.out.println("Hello");
+		//		}
+		//		else
+		//		{
+		//			System.out.println("Hi");
+		//		}
+
+
+
+		// Scanner sc = new Scanner(System.in);
+
+		//	       char ch = sc.next().charAt(0);
+		//
+		//	       if(ch>=65 && ch<=90)
+		//	       {
+		//	           System.out.println("1");
+		//	       }else if(ch>=97 && ch<=122){
+		//	           System.out.println("0");
+		//	       }else{
+		//	           System.out.println("-1");
+		//	       }
+
+
+		//		 int i=0;
+		//		   while(i<10)
+		//		   {
+		//		       i=i+1;
+		//		       System.out.print(i);
+		//		       i=i+1;
+		//		   }
+
+
+		//		int x=5;
+		//	    int y=5;
+		//	    while((x=5)==y)
+		//	    {
+		//	        System.out.println("Hello");
+		//	        x++;
+		//	        y++;
+		//	    }
+
+		//		int s = sc.nextInt();
+		//		int e = sc.nextInt();
+		//		int w = sc.nextInt();
+		//
+		//		double	c = 0.0;
+		//		
+		//        int i = s;
+		//
+		//		while(i<=e)
+		//		{
+		//		   c =   (i-32)/1.8000;
+		//			System.out.println(i+" "+(int)c);
+		//			i = i+w;
+		//		}
+		//		
+
+
+
+
+
+		//	      int basic = sc.nextInt();
+		//		  char grade = sc.next().charAt(0);
+		//
+		//		  double hra = 0.2*basic;
+		//		  double da = 0.5*basic;
+		//		  double pf = 0.11*basic;
+		//
+		//		  double totalSalary = 0;
+		//
+		//		  if(grade == 'A')
+		//		  {
+		//	          totalSalary = basic+hra+da+1700-pf;
+		//		  }else if(grade == 'B')
+		//		  {
+		//			  totalSalary = basic+hra+da+1500-pf;
+		//		  }else{
+		//			  totalSalary = basic+hra+da+1300-pf;
+		//		  }
+		//
+		//		
+		//		  System.out.println(Math.round(totalSalary));
+		//		
+		//		
+
+		//		
+		//
+		//		 int n = sc.nextInt();
+		//
+		//	    int i=1;
+		//		 while(i<=10)
+		//		 {
+		//			 System.out.println(i*n);
+		//			 i++;
+		//		 }
+
+
+//		int n = sc.nextInt();
+//
+//		int evenSum = 0;
+//		int oddSum = 0;
+//
+//		while(n!=0)
+//		{
+//			int rem = n%10;
+//			if(rem%2==0)
+//			{
+//				evenSum = evenSum +rem; 
+//			}else {
+//				oddSum = oddSum + rem;
+//			}
+//			n = n/10;
+//		}
+//
+//		System.out.print(evenSum+" "+oddSum);
+		
+//		int n = sc.nextInt();
+//				
+//		for (int i = 2; i < n; ++i) {
+//	         if (n % i == 0) {
+//	        	
+//	              System.out.print(i + " ");
+//	         }
+//	      }
+
+		
+//		 int x = sc.nextInt();
+//	        int n = sc.nextInt();
+//
+//	        int i=0,pow=1;
+//
+//	        while(i<n)
+//	        {
+//	          pow = pow*x;
+//	          i++;
+//	        }
+//
+//	      System.out.println(pow);
+
+		
+//		 int n=sc.nextInt();
+//		    boolean isprime=true;
+//		    if(n%2==0)
+//		        isprime=false;
+//		    int i=3;
+//		    while(isprime&&i<n)
+//		    {
+//		        isprime=!(n%i==0);
+//		        i+=2;
+//		    }
+//		    if(isprime)
+//		    {
+//		        System.out.println("Prime");
+//		    }
+//		    else
+//		    {
+//		        System.out.println("Composite");
+//		    }
+
+		
+//		int i=10;
+//	    while((i=i-1)>0)
+//	    {
+//	        System.out.print(i);
+//	        if(i%5==0)
+//	            return;
+//	    }
+
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//
+//           int j=1;
+//		   while(j<=n)
+//		   {
+//			   System.out.print(n);
+//			   j++;
+//		   }
+//          System.out.println();
+//		   i++;
+//		   
+//		}
+		
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//
+//           int j=1;
+//		   while(j<=i)
+//		   {
+//			   System.out.print(j);
+//			   j++;
+//		   }
+//          System.out.println();
+//		   i++;
+//		   
+//		}
+		
+//		int n = sc.nextInt();
+//
+//		int i=1,p=1;
+//
+//		while(i<=n)
+//		{
+//
+//           int j=1;
+//		   while(j<=i)
+//		   {
+//			   System.out.print(p);
+//			   p++;
+//			   j++;
+//		   }
+//          System.out.println();
+//		   i++;
+//		   
+//		}
+		
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//
+//           int j=1;
+//           int p = i;
+//		   while(j<=i)
+//		   {
+//			   System.out.print(p);
+//			   p++;
+//			   j++;
+//		   }
+//          System.out.println();
+//		   i++;
+//		   
+//		}
+		
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//
+//           int j=1;
+//          
+//		   while(j<=i)
+//		   {
+//			   System.out.print("*");
+//		
+//			   j++;
+//		   }
+//          System.out.println();
+//		   i++;
+//		   
+//		}
+		
+//		 int n = sc.nextInt();
+//
+//		  int i=1;
+//
+//		  while(i<=n)
+//		  {
+//
+//	        int j=1;
+//
+//			while(j<=i)
+//			{
+//				System.out.print(i);
+//				j++;
+//			}
+//			System.out.println();
+//			i++;
+//
+//
+//		  }
+		
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//			int j=1;
+//           int p=i;
+//			while(j<=i)
+//			{
+//				System.out.print(p);
+//				p--;
+//				j++;
+//			}
+//			System.out.println();
+//			i++;
+//		}
+
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//			int j=1;
+//			while(j<=n)
+//			{
+//				System.out.print((char)('A'+j-1));
+//				
+//				j++;
+//			}
+//			System.out.println();
+//			i++;
+//		}
+
+		
+//		 int n = sc.nextInt();
+//
+//		  int i=1;
+//
+//		  while(i<=n)
+//		  {
+//	         
+//			  int j=1;
+//
+//			  while(j<=i)
+//			  {
+//				  System.out.print((char)('A'+i-1));
+//				  j++;
+//			  }
+//			  System.out.println();
+//			  i++;
+//
+//		  }
+		
+		
+//		 int n = sc.nextInt();
+//
+//		  int i=1;
+//
+//		  while(i<=n)
+//		  {
+//	         
+//			  int j=1;
+//              int p=i;
+//			  while(j<=n)
+//			  {
+//				  System.out.print((char)('A'+p-1));
+//				  p++;
+//				  j++;
+//			  }
+//			  System.out.println();
+//			  i++;
+//
+//		  }
+		
+		
+		
+//		 int n = sc.nextInt();
+//
+//		  int i=1;
+//
+//		  while(i<=n)
+//		  {
+//	         
+//			   int j=1;
+//              int p = 0;
+//			  while(j<=i)
+//			  {
+//				  char ch = (char)('A'+n-i+p);
+//				  p++;
+//				  System.out.print(ch);
+//				
+//				  j++;
+//			  }
+//			  System.out.println();
+//			  i++;
+//
+//		  }
+
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//			int space = 1;
+//			while(space<=(n-i))
+//			{
+//				System.out.print(' ');
+//				space++;
+//			}
+//
+//	      int stars = 1;
+//	       while(stars<=i)
+//		   {
+//			   System.out.print(stars);
+//			   	stars++;
+//		   }
+//
+//			System.out.println();
+//			i++;
+//		}
+
+		
+		
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//			
+//			 int stars = 1;
+//			 int p=n;
+//		       while(stars<=n-i+1)
+//			   {
+//				   System.out.print(p-i+1);
+//				   	stars++;
+//			   }
+//                 
+//			int space = 1;
+//			while(space<=i)
+//			{
+//				System.out.print(' ');
+//				space++;
+//			}
+//
+//			System.out.println();
+//
+//			i++;
+//		}
+
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//		       
+//					int space = 1;
+//					while(space<=n-i)
+//					{
+//						System.out.print(' ');
+//						space++;
+//					}
+//
+//			 int num1 = 1;
+//		       while(num1<=i)
+//			   {
+//				   System.out.print(num1);
+//				   	num1++;
+//			   }
+//          
+//		       int num2 = 1;
+//		       while(num2<=i-1)
+//			   {
+//				   System.out.print(num2);
+//				   	num2++;
+//			   }
+//          
+//			System.out.println();
+//
+//			i++;
+//		}
+
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//		       
+//					int space = 1;
+//					while(space<=n-i)
+//					{
+//						System.out.print(' ');
+//						space++;
+//					}
+//
+//			 int num1 = 1;
+//		       while(num1<=i)
+//			   {
+//				   System.out.print("*");
+//				   	num1++;
+//			   }
+//          
+//		       int num2 = 1;
+//		       while(num2<=i-1)
+//			   {
+//				   System.out.print("*");
+//				   	num2++;
+//			   }
+//          
+//			System.out.println();
+//
+//			i++;
+//		}
+
+		
+//		int n = sc.nextInt();
+//
+//		int i=1;
+//
+//		while(i<=n)
+//		{
+//		       
+//			  int space = 1;
+//			  while(space<=n-i)
+//			  {
+//				System.out.print(' ');
+//				space++;
+//			  }
+//
+//			 int num1 = 1;
+//			 int p=i;
+//		       while(num1<=i)
+//			   {
+//				   System.out.print(p);
+//				   p++;
+//				   	num1++;
+//			   }
+//          
+//		       int num2 = 1;
+//		       int q=p-2;
+//		       while(num2<=i-1)
+//			   {
+//				   System.out.print(q);
+//				   q--;
+//				   	num2++;
+//			   }
+//          
+//			System.out.println();
+//
+//			i++;
+//		}
+
+		
+//		int n = sc.nextInt();
+//
+//		int i = 1;
+//		
+//		int br = n/2+1;
+//
+//		while (i <= n) {
+//
+//			if (i <= br) {
+//				int space = 1;
+//				while (space <= n / 2 - i + 1) {
+//					System.out.print(" ");
+//					space++;
+//				}
+//
+//				int num1 = 1;
+//				while (num1 <= i) {
+//					System.out.print("*");
+//					num1++;
+//				}
+//
+//				int num2 = 1;
+//				while (num2 <= i - 1) {
+//					System.out.print("*");
+//					num2++;
+//				}
+//			}
+//			else{
+//				int space = 1;
+//				
+//				while(space<=i-(n/2+1))
+//				{
+//					System.out.print(" ");
+//					space++;
+//				}
+//				
+//				int stars = i;
+//				while(stars<=n)
+//				{
+//					System.out.print("*");
+//					stars++;
+//				}
+//				
+//				int num2 = 1;
+//				while(num2<=n-i)
+//				{
+//					System.out.print("*");
+//					num2++;
+//				}
+//			}
+//
+//			System.out.println();
+//
+//			i++;
+//		}
+
+		
+		
+//		int n = sc.nextInt();
+//		int i=1;
+//		int j;
+//		while(i<=n)
+//		{
+//		j=1;
+//		while(j++<=i)
+//		 
+//		{
+//		System.out.print(j-1);
+//		 
+//		}
+//		 
+//		System.out.println();
+//		i++;
+//		} 
+//		i=n-1;
+//		while(i>0)
+//		{
+//		j=1;
+//		while(j++<=i)
+//		 
+//		{
+//		System.out.print(j-1);
+//		 
+//		}
+//		 
+//		System.out.println();
+//		i--;
+//		} 
+		
+		
+		
+		int n = sc.nextInt();
+		
+				int i=1;
+		        System.out.println("*");
+		        
+				while (i <= (2 * n - 1)) {
+					if (i <= (n)) {
+						System.out.print("*");
+						int num1 = 1;
+						int p = 1;
+						while (num1 <= i) {
+							System.out.print(p);
+							p++;
+							num1++;
+						}
+
+						int num2 = 1;
+						int q = p - 2;
+						while (num2 <= i - 1) {
+							System.out.print(q);
+							q--;
+							num2++;
+						}
+
+						System.out.println("*");
+
+					}else {
+						System.out.print("*");
+						
+						int j=i;
+						int p=i-3;
+						while(j<=i)
+						{
+							System.out.print(p);
+							p--;
+							j++;
+						}
+						
+						int num2 = 1;
+						int q = p -2;
+						while (num2 <= (i - j)) {
+							System.out.print(q);
+							q--;
+							num2++;
+						}
+						System.out.println("*");
+						
+					}
+		 
+					i++;
+				}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+
+
+
+
+
+
+
+
+
+	}
+
+}
